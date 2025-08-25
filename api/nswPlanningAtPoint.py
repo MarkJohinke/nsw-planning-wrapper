0bcf90a568acd8664c5644fd2d277a5daed97748
MarkJohinke <Mark.Johinke@JohinkeDevelopments.com.au>
2025-08-25 20:49:04 +1000

    Add files via upload

---
+++ api/nswPlanningAtPoint.py
@@ -0,0 +1,10 @@
+import json
+
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({"message": "Hello from NSW Planning Wrapper!"})
+    }
